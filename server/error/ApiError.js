6aad1fb429a22d67983cac7b63542739213b5095
wasdgfh <danik4878@gmail.com>
2025-05-26 19:22:01 +0300

    refactor: add conflict method to ApiError

--- server/error/ApiError.js
+++ server/error/ApiError.js
@@ -20,6 +20,11 @@ class ApiError extends Error {
     static notFound(message) {
         return new ApiError(404, message);
     }
+
+    static conflict(message) {
+        return new ApiError(409, message);
+    }
+
 }
 
 module.exports = ApiError;
