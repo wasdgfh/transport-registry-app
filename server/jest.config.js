96bf065beecdd0b83f465b927918dc414e82d341
wasdgfh <danik4878@gmail.com>
2025-05-28 18:57:47 +0300

    chore(jest): update setup and config for test environment

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   testEnvironment: 'node',
   setupFiles: ['./jest.setup.js'],
-  setupFilesAfterEnv: ['./jest.setup.after.js']
+  setupFilesAfterEnv: ['./jest.setup.after.js'],
+  maxWorkers: 1
 };
--- server/jest.setup.after.js
+++ server/jest.setup.after.js
@@ -2,8 +2,10 @@ const sequelize = require('./db');
 
 beforeAll(async () => {
   await sequelize.sync({ force: true });
+  jest.spyOn(console, 'error').mockImplementation(() => {});
 });
 
 afterAll(async () => {
   await sequelize.close(); 
+  console.error.mockRestore();
 });
